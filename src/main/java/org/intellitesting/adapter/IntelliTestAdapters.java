f813204f536d1e870cf152f4ed34c9a95cc40ee2
Renan Oliveira <renandemelo@gmail.com>
2013-10-07 18:18:33 -0300

    removing warnings

--- src/main/java/org/intellitesting/adapter/IntelliTestAdapters.java
+++ src/main/java/org/intellitesting/adapter/IntelliTestAdapters.java
@@ -2,6 +2,7 @@ package org.intellitesting.adapter;
 
 import java.util.ArrayList;
 
+@SuppressWarnings("serial")
 public class IntelliTestAdapters extends ArrayList<IntelliTestAdapter>{
 	
 	public IntelliTestAdapters(Class<?>[] tests) {
