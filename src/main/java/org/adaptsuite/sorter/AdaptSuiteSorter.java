e3fd66b5426bf3ad703e894f2efaf145132b32aa
andre <andre@andre-Inspiron-5423.(none)>
2014-02-20 09:14:17 -0300

    included AdaptSuiteSorter

---
+++ src/main/java/org/adaptsuite/sorter/AdaptSuiteSorter.java
@@ -0,0 +1,37 @@
+package org.adaptsuite.sorter;
+
+
+import java.util.Queue;
+
+import org.adaptsuite.adapter.IntelliTestAdapter;
+import org.adaptsuite.suite.AdaptSuiteBuilder;
+
+import junit.framework.TestSuite;
+
+/*Ideia dessa classe é escolher o maior número de testes possíveis dentro do 
+ * tempo limite, e não mais pegar os primeiros testes até estourar o tempo.*/
+public class AdaptSuiteSorter {
+	
+	private Long maxTime;
+	private Long[] testTime;
+	private Long[] testFailure;
+	
+	public void chooseTests (AdaptSuiteBuilder suiteBuilder, TestSuite suite) {
+		
+		Queue<IntelliTestAdapter> testQueue = suiteBuilder.getTestQueue();
+		int queueSize = testQueue.size();
+		maxTime = suiteBuilder.getAvailableTimeMili();
+		constructTestTime(testQueue, queueSize);
+	}
+	
+	private void constructTestTime (Queue<IntelliTestAdapter> testQueue, int queueSize) {
+		
+		testTime = new Long[queueSize];
+		int i = 0;
+		
+		for (IntelliTestAdapter obj : testQueue) {
+			testTime[i++] = obj.getTime();
+		}
+	}
+
+}
