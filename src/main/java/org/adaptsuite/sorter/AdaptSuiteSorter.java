cf4a2c9779850cf246dd1536f95931be58a9dac0
andre <andre@andre-Inspiron-5423.(none)>
2014-03-11 11:33:30 -0300

    fixed some bugs

--- src/main/java/org/adaptsuite/sorter/AdaptSuiteSorter.java
+++ src/main/java/org/adaptsuite/sorter/AdaptSuiteSorter.java
@@ -9,7 +9,7 @@ public class AdaptSuiteSorter {
 		
 		
 		chosenTests = new boolean[queueSize];
-		knaspackTabble = new Long [queueSize][maxTime.intValue()];
+		knaspackTabble = new Long [queueSize + 1][maxTime.intValue() + 1];
 		TestsValue(testTime, testFailure, queueSize, maxTime);
 		ChooseTests(testTime, queueSize, maxTime);
 		return chosenTests;
@@ -20,15 +20,15 @@ public class AdaptSuiteSorter {
 		
 		Long a, b;
 		
-		for (int time = 0; time < maxTime; time++) {
+		for (int time = 0; time <= maxTime; time++) {
 			knaspackTabble[0][time] = (Long)0L;
 			
-			for (int i = 1; i < queueSize; i++) {
+			for (int i = 1; i <= queueSize; i++) {
 				a = knaspackTabble[i-1][time];
-				if (testTime[i].intValue() > time)
+				if (testTime[i-1].intValue() > time)
 					b = (Long)0L;
 				else
-					b = knaspackTabble[i-1][time - testTime[i].intValue()] + testFailure[i];
+					b = knaspackTabble[i-1][time - testTime[i-1].intValue()] + testFailure[i];
 				
 				knaspackTabble[i][time] = Max(a, b);
 			}
@@ -43,7 +43,7 @@ public class AdaptSuiteSorter {
 	
 	private void ChooseTests (Long[] testTime, int queueSize, Long maxTime) {
 		
-		Long time = maxTime - 1;
+		Long time = maxTime;
 		
 		for (int i = queueSize - 1; i >= 1; i--)
 			if(knaspackTabble[i][time.intValue()] == knaspackTabble[i-1][time.intValue()])
