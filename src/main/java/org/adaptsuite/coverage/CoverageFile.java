4c9b261fb709451049aeceec42b35a5c63f09f3f
Andre Pestana <pestana@haskell.linux.ime.usp.br>
2014-08-29 10:56:00 -0300

    Add a new test on the Coverage file

--- src/main/java/org/adaptsuite/coverage/CoverageFile.java
+++ src/main/java/org/adaptsuite/coverage/CoverageFile.java
@@ -1,9 +1,11 @@
 package main.java.org.adaptsuite.coverage;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CoverageFile {
@@ -26,9 +28,13 @@ public class CoverageFile {
 				for(String line; (line = br.readLine()) != null; ) {
 				    prop = line.split(" ");
 				    
-				    if(isTheRightTest(prop[0], testName))
+				    if(isTheRightTest(prop[0], testName)) {
+				    		br.close();
 				    		return Double.parseDouble(prop[1]);
+				    }
 				}
+				br.close();
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -36,9 +42,24 @@ public class CoverageFile {
 			System.out.println("Couldn't open the 'Coverage' file");
 		}
 		
+		addTest(testName);
 		return 1.0;
 		
 		
+
+	}
+
+	private void addTest(String testName) {
+		
+        try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+			
+			bw.write("\n" + testName + " 1.0");
+			bw.close();
+			
+		} catch (IOException e) {
+			System.out.println("Couldn't open the 'Coverage' file");
+		}
 
 	}
 
